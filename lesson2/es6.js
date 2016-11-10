d2a73da427dac419f8af99684a6837cc92bbf507
Qun Sun <qsun@esri.com>
2016-11-10 13:41:24 +0800

    add lesson2

---
+++ lesson2/es6.js
@@ -0,0 +1,9 @@
+//./node_modules/.bin/babel es6.js -o compiled.js
+//For Windows:
+//1. cd node_modules/.bin
+//2. babel ../../es6.js -o compiled.js
+
+//For Linux:
+//./node_modules/.bin/babel es6.js -o compiled.js
+
+const square = n => n * n;
