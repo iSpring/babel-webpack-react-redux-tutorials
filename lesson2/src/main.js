daaf83e2940838bf9dd196ecaba9e9e7a36cbe3f
Qun Sun <qsun@esri.com>
2016-11-10 16:44:11 +0800

    update lesson2

--- lesson2/src/main.js
+++ lesson2/src/main.js
@@ -1,4 +1,8 @@
 import Circle from './Circle';
+import Rectangle from './Rectangle';
 
 const circle = new Circle(1);
 console.log(circle.toString());
+
+const rectangle = new Rectangle(2, 1);
+console.log(rectangle.toString());
