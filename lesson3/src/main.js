06a5487ef3184584ae61e28e6fec2b141c9a6a0e
Qun Sun <qsun@esri.com>
2016-11-14 19:07:49 +0800

    add lesson3 for webpack

---
+++ lesson3/src/main.js
@@ -0,0 +1,9 @@
+import Circle from './Circle';
+import Rectangle from './Rectangle';
+//import './index.css';
+
+const circle = new Circle(1);
+console.log(circle.toString());
+
+const rectangle = new Rectangle(2, 1);
+console.log(rectangle.toString());
---
+++ lesson3/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/main"
+  },
+  output: {
+    path: path.join(__dirname, "buildOutput"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel'
+      // query: {
+      //   presets: ['es2015']
+      // }
+    }]
+  }
+};
