e27137fdcf7e5f47f6ceda63c7fb33dc8f6ac7e4
Qun Sun <qsun@esri.com>
2016-11-15 13:03:26 +0800

    add lesson4 for advanced-webpack-lesson

---
+++ lesson4/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/main"
+  },
+  output: {
+    path: path.join(__dirname, "buildOutput"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, "src"),
+        path.resolve(__dirname, "node_modules/MyShape")
+      ],
+      exclude: [
+        path.resolve(__dirname, "node_modules/MyShape/index.js")
+      ],
+      loader: 'babel'
+      // query: {
+      //   presets: ['es2015']
+      // }
+    }, {
+      test: /\.css$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'style!css'//必须先经过css-loader处理，然后经过style-loader处理
+    }]
+  }
+};
