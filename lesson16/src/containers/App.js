abee1800776f319eb5f3107543fcaebaac422a8b
Qun Sun <qsun@esri.com>
2016-11-25 11:39:05 +0800

    make lesson16 work with mapDispatchToProps

--- lesson16/src/containers/App.js
+++ lesson16/src/containers/App.js
@@ -9,31 +9,41 @@ import * as stackOverflowActions from '../actions/stackOverflow';
 class App extends Component{
 
 	render(){
-		//dispatch, gitHubProps, stackOverflowProps都通过connect()方法注入进来
-		const {dispatch, gitHubProps, stackOverflowProps} = this.props;
+		console.log("App render props", this.props);
+		//gitHubProps和stackOverflowProps是通过mapStateToProps()和connect()方法注入进来的
+		//onGitHubInputChange、onGitHubSearch、onStackOverflowInputChange和onStackOverflowSearch是通过mapDispatchToProps()和connect()方法注入进来的
+		const {
+			gitHubProps,
+			onGitHubInputChange,
+			onGitHubSearch,
+
+			stackOverflowProps,
+			onStackOverflowInputChange,
+			onStackOverflowSearch
+		} = this.props;
 
 		return (
 			<div>
 		      <GitHubSearch
-		      	keyword={gitHubProps.keyword}
-		      	loading={gitHubProps.loading}
-		      	items={gitHubProps.items}
-		      	onInputChange={(keyword) => {dispatch(gitHubActions.gitHubInputChange(keyword))}}
-		      	onSearch={(keyword) => {dispatch(gitHubActions.gitHubFetchData(keyword))}}
+		      	keyword = {gitHubProps.keyword}
+		      	loading = {gitHubProps.loading}
+		      	items = {gitHubProps.items}
+		      	onInputChange = {onGitHubInputChange}
+		      	onSearch = {onGitHubSearch}
 		      />
 		      <StackOverflowSearch
-		      	keyword={stackOverflowProps.keyword}
-		      	loading={stackOverflowProps.loading}
-		      	items={stackOverflowProps.items}
-		      	onInputChange={(keyword) => {dispatch(stackOverflowActions.stackOverflowInputChange(keyword))}}
-		      	onSearch={(keyword) => dispatch(stackOverflowActions.stackOverflowFetchData(keyword))}
+		      	keyword = {stackOverflowProps.keyword}
+		      	loading = {stackOverflowProps.loading}
+		      	items = {stackOverflowProps.items}
+		      	onInputChange = {onStackOverflowInputChange}
+		      	onSearch = {onStackOverflowSearch}
 		      />
 		    </div>
 		);
 	}
 }
 
-//将state映射到组件GitHubSearch和StackOverflowSearch的props参数
+//将state映射到UI组件GitHubSearch和StackOverflowSearch的props参数
 function mapStateToProps(state){
 	return {
 		gitHubProps: {
@@ -45,4 +55,14 @@ function mapStateToProps(state){
 	};
 }
 
-export default connect(mapStateToProps)(App);
+//将UI组件GitHubSearch和StackOverflowSearch的操作映射成dispatch相应的action
+function mapDispatchToProps(dispatch){
+	return {
+		onGitHubInputChange: (keyword) => {dispatch(gitHubActions.gitHubInputChange(keyword))},
+		onGitHubSearch: (keyword) => {dispatch(gitHubActions.gitHubFetchData(keyword))},
+		onStackOverflowInputChange: (keyword) => {dispatch(stackOverflowActions.stackOverflowInputChange(keyword))},
+		onStackOverflowSearch: (keyword) => dispatch(stackOverflowActions.stackOverflowFetchData(keyword))
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
