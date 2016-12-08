6bed69ee23e57cfb916feb5d5bb5324ddd2fe015
Qun Sun <qsun@esri.com>
2016-12-08 17:46:54 +0800

    add Map Component for lesson17

---
+++ lesson17/src/components/Map/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Map extends Component{
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>Map Component</div>
+    );
+  }
+}
+
+export default Map;
