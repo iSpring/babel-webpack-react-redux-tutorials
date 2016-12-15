b3f4da901af8f8a6eb8306f1246b7aff08a6d290
Qun Sun <qsun@esri.com>
2016-12-15 15:04:30 +0800

    update webpack-common-chunk

--- webpack-common-chunk/webpack.config.js
+++ webpack-common-chunk/webpack.config.js
@@ -12,9 +12,36 @@ module.exports = {
 		path: path.join(__dirname, "buildOutput"),
 		filename: "[name].js"
 	},
+	// plugins: [
+	// 	new webpack.optimize.CommonsChunkPlugin({
+	// 		// 顺序很重要，最后一个用于存储webpack runtime的代码
+	// 		names: ["webpack-runtime"]
+	// 	})
+	// ]
+	// plugins: [
+	// 	new webpack.optimize.CommonsChunkPlugin({
+	// 		// 顺序很重要，最后一个用于存储webpack runtime的代码
+	// 		names: ["webpack-runtime"],
+	// 		minChunks: 3
+	// 	})
+	// ]
+	// plugins: [
+	// 	new webpack.optimize.CommonsChunkPlugin({
+	// 		// 顺序很重要，最后一个用于存储webpack runtime的代码
+	// 		names: ["webpack-runtime"],
+	// 		minChunks: 2
+	// 	})
+	// ]
+	// plugins: [
+	// 	new webpack.optimize.CommonsChunkPlugin({
+	// 		// 顺序很重要，webpack-runtime.js用于存储webpack runtime的代码，common.js用于存储其他公共模块
+	// 		names: ["common", "webpack-runtime"],
+	// 		minChunks: 2
+	// 	})
+	// ]
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin({
-			// The order of this array matters
+			// 顺序很重要，vendor.js用于存储webpack runtime的代码，common.js用于存储其他公共模块
 			names: ["common", "vendor"],
 			minChunks: 2
 		})
