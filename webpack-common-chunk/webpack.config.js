e12a7085edb0411e8f659ad3ee57e45a5f1005e4
Qun Sun <qsun@esri.com>
2016-12-15 15:26:36 +0800

    update webpack.config.js of webpack-common-chunk

--- webpack-common-chunk/webpack.config.js
+++ webpack-common-chunk/webpack.config.js
@@ -14,34 +14,38 @@ module.exports = {
 	},
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
-	// 		// 顺序很重要，最后一个用于存储webpack runtime的代码
-	// 		names: ["webpack-runtime"]
+	// 		// 用于存储webpack runtime的代码
+	// 		name: "webpack-runtime"
 	// 	})
 	// ]
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
-	// 		// 顺序很重要，最后一个用于存储webpack runtime的代码
-	// 		names: ["webpack-runtime"],
+	// 		// 用于存储webpack runtime的代码以及至少被3个entry都使用的公共模块
+	// 		name: "webpack-runtime",
 	// 		minChunks: 3
 	// 	})
 	// ]
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
-	// 		// 顺序很重要，最后一个用于存储webpack runtime的代码
-	// 		names: ["webpack-runtime"],
+	// 		// 用于存储webpack runtime的代码以及至少被2个entry都使用的公共模块
+	// 		name: "webpack-runtime",
 	// 		minChunks: 2
 	// 	})
 	// ]
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
-	// 		// 顺序很重要，webpack-runtime.js用于存储webpack runtime的代码，common.js用于存储其他公共模块
+	// 		//顺序很重要
+	// 		//common.js用于至少被2个entry都使用的公共模块
+	// 		//webpack-runtime.js用于存储webpack runtime的代码
 	// 		names: ["common", "webpack-runtime"],
 	// 		minChunks: 2
 	// 	})
 	// ]
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin({
-			// 顺序很重要，vendor.js用于存储webpack runtime的代码，common.js用于存储其他公共模块
+			//顺序很重要
+			//common.js用于至少被2个entry都使用的公共模块
+			//vendor本身是一个entry，其包含vendor1和vendor2的代码，在此基础上vendor.js还会包含webpack runtime的代码
 			names: ["common", "vendor"],
 			minChunks: 2
 		})
