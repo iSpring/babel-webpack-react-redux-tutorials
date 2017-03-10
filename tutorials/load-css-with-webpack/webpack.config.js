83b7dfd037133c652e2babe42221fd9126e722b3
Qun Sun <qsun@esri.com>
2017-03-10 18:37:26 +0800

    update load-css-with-webpack

--- tutorials/load-css-with-webpack/webpack.config.js
+++ tutorials/load-css-with-webpack/webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
     entry: {
-        main: "./src/main"
+        main: "./index.js"
     },
     output: {
         path: path.join(__dirname, "buildOutput"),
@@ -10,13 +10,6 @@ module.exports = {
     },
     module: {
         loaders: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel'
-                // query: {
-                //   presets: ['es2015']
-                // }
-        }, {
             test: /\.css$/,
             exclude: /(node_modules|bower_components)/,
             loader: 'style!css' //必须先经过css-loader处理，然后经过style-loader处理
