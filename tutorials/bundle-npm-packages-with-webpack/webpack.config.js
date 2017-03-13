953f25efef058a2c364a54a1753dbbba9007b1cc
iSpring <QSun@esri.com>
2017-03-13 23:25:54 +0800

    update

--- tutorials/bundle-npm-packages-with-webpack/webpack.config.js
+++ tutorials/bundle-npm-packages-with-webpack/webpack.config.js
@@ -1,29 +1,22 @@
 var path = require("path");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/main.js"),
-  output: {
-    path: path.resolve(__dirname, "buildOutput"),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      include: [
-        path.resolve(__dirname, "src"),
-        path.resolve(__dirname, "node_modules/MyShape")
-      ],
-      exclude: [
-        path.resolve(__dirname, "node_modules/MyShape/index.js")
-      ],
-      loader: 'babel'
-    }]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack',
-      filename: 'index.html'
-    })
-  ]
+    entry: path.resolve(__dirname, "src/main.js"),
+    output: {
+        path: path.resolve(__dirname, "buildOutput"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            include: [
+                path.resolve(__dirname, "src"),
+                path.resolve(__dirname, "node_modules/MyShape")
+            ],
+            exclude: [
+                path.resolve(__dirname, "node_modules/MyShape/index.js")
+            ],
+            loader: 'babel'
+        }]
+    }
 };
