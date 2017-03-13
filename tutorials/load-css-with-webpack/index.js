05669b46e8b0273997314706a5a27c4668051643
Qun Sun <qsun@esri.com>
2017-03-13 10:55:56 +0800

    update

--- tutorials/load-css-with-webpack/index.js
+++ tutorials/load-css-with-webpack/index.js
@@ -1,8 +1,8 @@
-import './a.css';
-import './b.scss';
-import './c.css';
+// import './a.css';
+// import './b.scss';
+// import './c.css';
 
-// import './index.css';
+import './index.css';
 
 //Webpack import vs require
 //https://github.com/webpack/webpack/issues/1973#issuecomment-185744317
