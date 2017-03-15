93a6f2843b69792aff94fa8cfc88a41b7e4c0c69
Qun Sun <qsun@esri.com>
2017-03-15 13:04:30 +0800

    add use-extract-text-webpack-plugin

---
+++ tutorials/use-extract-text-webpack-plugin/postcss.config.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+    plugins: [
+        require("precss")(),
+        require("autoprefixer")()
+    ]
+}
---
+++ tutorials/use-extract-text-webpack-plugin/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.join(__dirname, "buildOutput"),
+        filename: "bundle.js"
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel'
+        }, {
+            test: /\.css$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'style!css!postcss'
+        }, {
+            test: /\.scss$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'style!css!postcss!sass'
+        }, {
+            test: /\.less$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'style!css!postcss!less'
+        }]
+    }
+};
