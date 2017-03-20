0fb673180c123365ab1590f810b37260e436195a
iSpring <QSun@esri.com>
2017-03-20 22:16:26 +0800

    update

--- tutorials/webpack-htmlwebpackplugin/src/main.js
+++ tutorials/webpack-htmlwebpackplugin/src/main.js
@@ -1,6 +1,6 @@
 import Circle from './Circle';
 import Rectangle from './Rectangle';
-import './index.css';
+// import './index.css';
 
 const circle = new Circle(1);
 console.log(circle.toString());
--- tutorials/webpack-htmlwebpackplugin/webpack.config.js
+++ tutorials/webpack-htmlwebpackplugin/webpack.config.js
@@ -1,41 +1,40 @@
 var path = require("path");
+
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: "./src/main"
-  },
-  output: {
-    path: path.join(__dirname, "buildOutput"),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      loader: 'style!css'
-    }]
-  },
-  plugins: [
-    // new HtmlWebpackPlugin()
+    entry: {
+        main: "./src/main"
+    },
+    output: {
+        path: path.join(__dirname, "buildOutput"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }]
+    },
+    plugins: [
+        // new HtmlWebpackPlugin()
 
-    new HtmlWebpackPlugin({
-      title: 'Use HtmlWebpackPlugin',
-      filename: 'index.html',
-      favicon: './logo.png',
-      inject: 'body',
-      hash: true
-    })
+        new HtmlWebpackPlugin({
+            title: 'Use HtmlWebpackPlugin',
+            filename: 'index.html',
+            favicon: '../images/logo.png',
+            inject: 'body',
+            hash: true
+        })
 
-    // new HtmlWebpackPlugin({
-    //   template: '!!ejs!./template.html',
-    //   description: 'Use HtmlWebpackPlugin',
-    //   bootstrap: 'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-    //   jQuery: 'http://code.jquery.com/jquery-1.12.4.min.js'
-    // })
-  ]
+        // new HtmlWebpackPlugin({
+        //   template: '!!ejs!./template.html',
+        //   description: 'Use HtmlWebpackPlugin',
+        //   bootstrap: 'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+        //   jQuery: 'http://code.jquery.com/jquery-1.12.4.min.js'
+        // })
+    ]
 };
