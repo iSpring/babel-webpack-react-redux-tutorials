4005fc7d50e7e83b5e9b7485b731183eddba1e3b
iSpring <QSun@esri.com>
2017-03-22 23:53:21 +0800

    update

--- tutorials/webpack-code-splitting/index.js
+++ tutorials/webpack-code-splitting/index.js
@@ -1,6 +1,12 @@
 var a = require("./a");
 var b = require("./b");
+
+console.log(a);
+console.log(b);
+
 require.ensure([], function(require) {
     var c = require("./c");
     var d = require("./d");
+    console.log(c);
+    console.log(d);
 });
--- tutorials/webpack-code-splitting/webpack.config.js
+++ tutorials/webpack-code-splitting/webpack.config.js
@@ -1,11 +1,15 @@
 var path = require("path");
 
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-	entry: {
-		main: path.join(__dirname, "main.js")
-	},
-	output: {
-		path: path.join(__dirname, "buildOutput"),
-		filename: "[name].js"
-	}
+    entry: path.join(__dirname, "index.js"),
+    output: {
+        path: path.join(__dirname, "buildOutput"),
+        filename: "[name].js"
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 };
