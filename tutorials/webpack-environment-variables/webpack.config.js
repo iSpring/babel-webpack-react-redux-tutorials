6bf3e5627d4fd41dd34a63df0687ecdcfd0a40a4
Qun Sun <qsun@esri.com>
2017-03-27 12:24:31 +0800

    add webpack-environment-variables

---
+++ tutorials/webpack-environment-variables/webpack.config.js
@@ -0,0 +1,16 @@
+﻿var path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.join(__dirname, "buildOutput"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel'
+    }]
+  }
+};
