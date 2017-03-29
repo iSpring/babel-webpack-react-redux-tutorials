e900547e090253f95691e8663caa952717325339
Qun Sun <qsun@esri.com>
2017-03-29 12:55:44 +0800

    update webpack-common-chunk

--- tutorials/webpack-common-chunk/webpack.config.js
+++ tutorials/webpack-common-chunk/webpack.config.js
@@ -8,44 +8,50 @@ module.exports = {
 		pageB: "./pageB",
 		pageC: "./pageC"
 	},
+
 	output: {
 		path: path.join(__dirname, "buildOutput"),
 		filename: "[name].js"
 	},
+
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
-	// 		// 用于存储webpack runtime的代码
+	// 		// webpack-runtime.js用于存储webpack runtime的代码以及被所有entry都是用的公共模块，本例中webpack-runtime.js = webpack runtime
 	// 		name: "webpack-runtime"
 	// 	})
 	// ]
+
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
-	// 		// 用于存储webpack runtime的代码以及至少被3个entry都使用的公共模块
+	// 		// webpack-runtime.js用于存储webpack runtime的代码以及至少被3个entry都使用的公共模块，即webpack-runtime.js = webpack runtime + utility2.js
 	// 		name: "webpack-runtime",
 	// 		minChunks: 3
 	// 	})
 	// ]
+
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
-	// 		// 用于存储webpack runtime的代码以及至少被2个entry都使用的公共模块
+	// 		// webpack-runtime.js用于存储webpack runtime的代码以及至少被2个entry都使用的公共模块，即webpack-runtime.js = webpack runtime + utility2.js + utility3.js
 	// 		name: "webpack-runtime",
 	// 		minChunks: 2
 	// 	})
 	// ]
+
 	// plugins: [
 	// 	new webpack.optimize.CommonsChunkPlugin({
 	// 		//顺序很重要
-	// 		//common.js用于至少被2个entry都使用的公共模块
-	// 		//webpack-runtime.js用于存储webpack runtime的代码
+	// 		//common.js用于至少被2个entry都使用的公共模块，即common.js = utility2.js + utility3.js
+	// 		//webpack-runtime.js用于存储webpack runtime的代码，即webpack-runtime.js = webpack runtime
 	// 		names: ["common", "webpack-runtime"],
 	// 		minChunks: 2
 	// 	})
 	// ]
+
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin({
 			//顺序很重要
-			//common.js用于至少被2个entry都使用的公共模块
-			//vendor本身是一个entry，其包含vendor1和vendor2的代码，在此基础上vendor.js还会包含webpack runtime的代码
+			//common.js用于至少被2个entry都使用的公共模块，即common.js = utility2.js + utility3.js
+			//vendor本身是一个entry，其包含vendor1和vendor2的代码，在此基础上vendor.js还会包含webpack runtime的代码，即vendor.js = webpack runtime + vendor1.js + vendor2.js
 			names: ["common", "vendor"],
 			minChunks: 2
 		})
