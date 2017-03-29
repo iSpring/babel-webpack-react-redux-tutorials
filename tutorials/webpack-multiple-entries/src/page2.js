568d9ee3524ad983bafda190342c511d4b06ac87
Qun Sun <qsun@esri.com>
2017-03-29 11:00:49 +0800

    update

---
+++ tutorials/webpack-multiple-entries/src/page2.js
@@ -0,0 +1,5 @@
+﻿import c from "./c.js";
+import d from "./d.js";
+
+console.log("module c: ", c);
+console.log("module d: ", d);
--- tutorials/webpack-multiple-entries/webpack.config.js
+++ tutorials/webpack-multiple-entries/webpack.config.js
@@ -2,17 +2,39 @@
 
 var webpack = require('webpack');
 
-module.exports = {
-    // entry: "./src/index.js",
-    // output: {
-    //     path: path.join(__dirname, "buildOutput"),
-    //     filename: "bundle.js"
-    // },
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-    entry: ["./src/a.js", "./src/b.js"],
+var htmlPlugin = new HtmlWebpackPlugin();
+
+module.exports = {
+    /*
+    //if entry is a single string, only one chunk file created, must set output.filename and output.chunkFilename is ignored
+    entry: "./src/page1.js",
     output: {
         path: path.join(__dirname, "buildOutput"),
-        filename: "bundle.js"
+        filename: "page1.bundle.js",//=>page1.bundle.js
+        // filename: "[name].bundle.js"//=>main.bundle.js
+        // filename: "[id].bundle.js"//=>0.bundle.js
+    },*/
+
+    /*
+    //if entry is an array, still only create one chunk file, must set output.filename and output.chunkFilename is ignored
+    entry: ["./src/page1.js", "./src/page2.js"],
+    output: {
+        path: path.join(__dirname, "buildOutput"),
+        filename: "page12.bundle.js"//=>page12.bundle.js
+        // filename: "[name].bundle.js"//=>main.bundle.js
+        // filename: "[id].bundle.js"//=>0.bundle.js
+    },*/
+
+    entry: {
+        page1: ["./src/page1.js"],
+        page2: ["./src/page2.js"]
+    },
+    output: {
+        path: path.join(__dirname, "buildOutput"),
+        // filename: "bundle.js"//=>bundle.js, only include page2.js
+        filename: "[id].[name].bundle.js"//=>0.page1.bundle.js(chunk id:0, chunck name:page1)  1.page2.bundle.js(chunck id:1, chunck name:page2)
     },
 
     module: {
@@ -21,4 +43,6 @@ module.exports = {
             loader: 'babel'
         }]
     },
+
+    plugins: [htmlPlugin]
 };
