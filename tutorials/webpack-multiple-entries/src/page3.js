72dfd9f1649a0b7cb3010ff678d8683835661ee0
Qun Sun <qsun@esri.com>
2017-03-29 12:14:01 +0800

    update

---
+++ tutorials/webpack-multiple-entries/src/page3.js
@@ -0,0 +1,12 @@
+﻿import a from "./a.js";
+import b from "./b.js";
+
+console.log("module a: ", a);
+console.log("module b: ", b);
+
+require.ensure(["./c.js", "./d.js"], function(){
+  const c = require("./c.js");
+  const d = require("./d.js");
+  console.log("module c: ", c);
+  console.log("module d: ", d);
+}, "cd");
--- tutorials/webpack-multiple-entries/webpack.config.js
+++ tutorials/webpack-multiple-entries/webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
     },*/
 
 
+    /*
     //If entry is an object, every key/value will create a chunk file. We can use key as [name]. We must set output.filename. In this case, output.chunkFilename is ignored.
     entry: {
         page1: ["./src/page1.js"],
@@ -38,7 +39,15 @@ module.exports = {
     output: {
         path: path.join(__dirname, "buildOutput"),
         // filename: "bundle.js"//=>bundle.js, only include page2.js
-        filename: "[id].[name].bundle.js"//=>0.page1.bundle.js(chunk id:0, chunck name:page1)  1.page2.bundle.js(chunck id:1, chunck name:page2)
+        filename: "[id].[name].bundle.js"//=>0.page1.bundle.js(chunk id: 0, chunck name: page1)  1.page2.bundle.js(chunck id:1, chunck name:page2)
+    },*/
+
+    entry: "./src/page3.js",
+    output: {
+        path: path.join(__dirname, "buildOutput"),
+        // filename: "page3.bundle.js"//page3.bundle.js(chunk id: 0, chunck name: main) includes webpack runtime, 1.page3.bundle.js(chunk id: 1, chunk name: cd) doesn't include webpack runtime
+        // filename: "[id]-[name].js"//0-main.js(chunk id: 0, chunk name: main) 1.1-cd.js(chunk id: 1, chunk name: cd)
+        filename: "page3.bundle.js", chunkFilename: "[id].[name].js"//page3.bundle.js(chunk id: 0, chunck name: main) 1.cd.js(chunk id: 1, chunk name: cd)
     },
 
     module: {
